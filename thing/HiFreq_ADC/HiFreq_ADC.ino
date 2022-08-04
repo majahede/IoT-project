c56f764963ea9762c4a52aa68dac781ee28f451c
Maja Hedegard <mh223pi@student.lnu.se>
2022-08-04 12:47:49 +0200

    add wifi setup

--- thing/HiFreq_ADC/HiFreq_ADC.ino
+++ thing/HiFreq_ADC/HiFreq_ADC.ino
@@ -1,10 +1,38 @@
 #include "DHTesp.h"
 #include "heltec.h"
+#include "WiFi.h"
+
+#define WIFI_NETWORK "wifi"
+#define WIFI_PASSWORD "password"
+#define WIFI_TIMEOUT_MS 20000
+
 DHTesp dht;
  
 float currentTemp;
 float currentHumidity;
- 
+
+void connectToWifi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+  Serial.print("Connecting to WiFi");
+
+  unsigned long startAttemptTime = millis();
+
+  while(WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS){
+    Serial.print(".");
+    delay(100);
+  }
+
+  if(WiFi.status()!= WL_CONNECTED) {
+    Serial.print("Failed!");
+    Serial.print(WiFi.status());
+    // take action
+  } else {
+    Serial.print("Connected!");
+    Serial.println(WiFi.localIP());
+  }
+}
+
 void displayReadingsOnOled() {
    
   String temperatureDisplay ="Temperature: " + (String)currentTemp +  "°C";
@@ -22,6 +50,9 @@ void displayReadingsOnOled() {
     
 void setup()
 {
+  Serial.begin(115200);
+  
+  connectToWifi();
  
   dht.setup(12, DHTesp::DHT11);
    
@@ -35,8 +66,6 @@ void setup()
   displayReadingsOnOled();
 }
  
- 
-    
 void loop()
 {
   float temperature = dht.getTemperature();
