3fdffc1fdafed4563914a6aa1388745845a08d3f
Maja Hedegard <mh223pi@student.lnu.se>
2022-08-05 12:36:43 +0200

    setup connection to influxdb

--- thing/HiFreq_ADC/HiFreq_ADC.ino
+++ thing/HiFreq_ADC/HiFreq_ADC.ino
@@ -1,37 +1,37 @@
 #include "DHTesp.h"
 #include "heltec.h"
-#include "WiFi.h"
 
-#define WIFI_NETWORK "wifi"
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti;
+#define DEVICE "ESP8266"
+#endif
+
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
+#define WIFI_SSID "ssid"
 #define WIFI_PASSWORD "password"
-#define WIFI_TIMEOUT_MS 20000
+
+#define INFLUXDB_URL "url"
+#define INFLUXDB_TOKEN "token"
+#define INFLUXDB_ORG "org"
+#define INFLUXDB_BUCKET "bucket"
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
+
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+// Data point
+Point sensor("wifi_status");
 
 DHTesp dht;
  
 float currentTemp;
 float currentHumidity;
-
-void connectToWifi() {
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi");
-
-  unsigned long startAttemptTime = millis();
-
-  while(WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS){
-    Serial.print(".");
-    delay(100);
-  }
-
-  if(WiFi.status()!= WL_CONNECTED) {
-    Serial.print("Failed!");
-    Serial.print(WiFi.status());
-    // take action
-  } else {
-    Serial.print("Connected!");
-    Serial.println(WiFi.localIP());
-  }
-}
 
 void displayReadingsOnOled() {
    
@@ -52,8 +52,32 @@ void setup()
 {
   Serial.begin(115200);
   
-  connectToWifi();
- 
+  // Setup wifi
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+
+  // Add tags
+  sensor.addTag("device", DEVICE);
+  sensor.addTag("SSID", WiFi.SSID());
+
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  // Check server connection
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  
   dht.setup(12, DHTesp::DHT11);
    
   currentTemp = dht.getTemperature();
@@ -76,6 +100,24 @@ void loop()
     currentHumidity = humidity;
     displayReadingsOnOled();
   }
-    
-  delay(300000);
+   // Store measured value into point
+  sensor.clearFields();
+  // Report RSSI of currently connected network
+  sensor.addField("rssi", WiFi.RSSI());
+  // Print what are we exactly writing
+  Serial.print("Writing: ");
+  Serial.println(client.pointToLineProtocol(sensor));
+  // If no Wifi signal, try to reconnect it
+  if (wifiMulti.run() != WL_CONNECTED) {
+    Serial.println("Wifi connection lost");
+  }
+  // Write point
+  if (!client.writePoint(sensor)) {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+
+  //Wait 10s
+  Serial.println("Wait 10s");
+  delay(10000);
 }
