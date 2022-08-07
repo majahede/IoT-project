975b807c872bca224764ab11f275fe9d64185b64
Maja Hedegard <mh223pi@student.lnu.se>
2022-08-07 15:13:27 +0200

    add influxdbService

--- app/Program.cs
+++ app/Program.cs
@@ -1,8 +1,11 @@
+using app.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddSingleton<InfluxDbService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
