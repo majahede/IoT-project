c2fe239369c12b6c50f615aca9389caab7782126
Maja Hedegard <mh223pi@student.lnu.se>
2022-11-02 19:21:32 +0100

    add endpoint to get current value

--- api/Controllers/SensorController.cs
+++ api/Controllers/SensorController.cs
@@ -25,6 +25,13 @@ public class SensorController : ControllerBase
     }
     
     [HttpGet]
+    [Route("temperature/current")]
+    public async Task<SensorDataModel> GetCurrentTemperature()
+    {    
+        return await _sensorDataService.GetCurrentSensorDataAsync("temperature");
+    }
+    
+    [HttpGet]
     [Route("humidity")]
     public async Task<List<SensorDataModel>> GetHumidityValues()
     {    
@@ -32,9 +39,23 @@ public class SensorController : ControllerBase
     }
     
     [HttpGet]
+    [Route("humidity/current")]
+    public async Task<SensorDataModel> GetCurrentHumidity()
+    {    
+        return await _sensorDataService.GetCurrentSensorDataAsync("humidity");
+    }
+    
+    [HttpGet]
     [Route("heat-index")]
     public async Task<List<SensorDataModel>> GetHeatIndexValues()
     {    
         return await _sensorDataService.GetSensorDataAsync("heat_index");
+    }
+    
+    [HttpGet]
+    [Route("heat-index/current")]
+    public async Task<SensorDataModel> GetCurrentHeatIndex()
+    {    
+        return await _sensorDataService.GetCurrentSensorDataAsync("heat_index");
     }
 }
